014519aac82e6498b138b661c3e3c5bb6cafe61c
bsrprathapreddy <65775804+bsrprathapreddy@users.noreply.github.com>
2020-06-23 09:19:41 +0530

    Add files via upload

---
+++ src/luv2code/ArrayListDemo.java
@@ -0,0 +1,64 @@
+package com.love2code;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Student> studen = new ArrayList<>();
+		studen.addAll(new Test().loadStudent());
+//		System.out.println(studen);
+		
+		if(!studen.isEmpty()) {
+			for(Student x : studen) {
+//				System.out.println(x.getName() + "  " + x.getRollNo());
+				String modify = x.getName().substring(0, 1);
+//				System.out.println(modify);
+				if(modify.equalsIgnoreCase("A")) {
+					System.out.println(x.getName());
+				}
+			}
+		}
+		
+		
+		List<Employee> employ = new ArrayList<>();
+		employ.addAll(Test.loadEmployee());
+		
+		if(!employ.isEmpty()) {
+			for(Employee x : employ) {
+//				System.out.println(x.getName() + "  " + x.getEid());
+				String modify = x.getName().substring(0,1);
+//				System.out.println(modify);
+				
+				if(modify.equalsIgnoreCase("a")) {
+					System.out.println(x.getName());
+				}
+			}
+		}
+		
+		
+		List<String> s = new ArrayList<>();
+		s.add("Apple");
+		s.add("Banana");
+		s.add("Ant");
+		s.add("Bat");
+		s.add("Animals");
+		s.add("Ball");
+		
+		System.out.println(s);
+	
+		if(!s.isEmpty()) {
+			for(String x : s) {
+				String modify = x.substring(0, 1);
+				if(modify.equalsIgnoreCase("A")) {
+					System.out.println(x);
+				}
+			}
+		}
+		
+	}
+
+}
+ 
